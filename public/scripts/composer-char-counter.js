66039ff4342caebf7a5a29aba7604e9d20bb4ffb
Brian Nguyen <97560651+brianguyen97@users.noreply.github.com>
2022-05-15 22:26:22 -0600

    Create composer-char-counter.js
    Function that makes counter red when character count is less than 0

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+  console.log("yes");
+  // --- our code goes here ---
+  $(".new-tweet-text").on("input", function () {
+    let maxLength = 140;
+    let currentLength = $(this).val().length;
+    let remainingCount = maxLength - currentLength;
+
+    if (remainingCount >= 0) {
+      $(".counter")
+        .addClass("counter")
+        .removeClass("counter-negative")
+        .text(remainingCount);
+    } else {
+      $(".counter")
+        .addClass("counter-negative")
+        .removeClass("counter-positive")
+        .text(remainingCount);
+    }
+  });
+});
