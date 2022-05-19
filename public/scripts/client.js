65da63f7cf5d325fbf6db51c34563c2dd02b8088
Brian Nguyen <97560651+brianguyen97@users.noreply.github.com>
2022-05-18 18:02:56 -0600

    Update client.js
    - Implement loadTweets function
    - Implement time-ago
    - Remove hard coded tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,31 +5,6 @@
  */
 
 $(document).ready(function () {
-  const data = [
-    {
-      user: {
-        name: "Newton",
-        avatars: "https://i.imgur.com/73hZDYK.png",
-        handle: "@SirIsaac",
-      },
-      content: {
-        text: "If I have seen further it is by standing on the shoulders of giants",
-      },
-      created_at: 1461116232227,
-    },
-    {
-      user: {
-        name: "Descartes",
-        avatars: "https://i.imgur.com/nlhLi3I.png",
-        handle: "@rd",
-      },
-      content: {
-        text: "Je pense , donc je suis",
-      },
-      created_at: 1461113959088,
-    },
-  ];
-
   const createTweetElement = function (data) {
     let $tweet = $(`
       <article class="tweets-container">
@@ -42,7 +17,7 @@ $(document).ready(function () {
         <div class="user-handle">${data.user.handle}</div>
         <div class="tweet-content">${data.content.text}
         </div>
-        <div class="time-stamp">${data.created_at}</div>
+        <div class="time-stamp">${timeago.format(data.created_at)}</div>
         <div class="grid-icons">
           <div class="flex-icons">
             <i class="fa-solid fa-flag"></i>
@@ -61,5 +36,29 @@ $(document).ready(function () {
     }
   };
 
-  renderTweets(data);
+  $(".new-tweet-container").submit(function (e) {
+    e.preventDefault();
+    const data = $(this).serialize();
+    $.ajax({
+      type: "POST",
+      url: "/tweets",
+      data,
+    });
+  });
+
+  const loadTweets = function () {
+    $.ajax({
+      type: "GET",
+      url: "/tweets",
+      dataType: "json",
+    })
+      .then((data) => {
+        renderTweets(data);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+
+  loadTweets();
 });
